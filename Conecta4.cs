08b921af8ac9dade28b787ab3bdd45fd9d644b5a
LailaXD54 <lilingch@ucm.es>
2025-04-24 17:33:04 +0200

    carpetas

---
+++ Conecta4.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace Conecta4
+{
+    public class Conecta4
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Tablero
+    {
+        private TipoCasilla[,] tablero;
+        private int filas;
+        private int columnas;
+        public Tablero(int filas = 6, int columnas = 7)
+        {
+            this.filas = filas;
+            this.columnas = columnas;
+            tablero = new TipoCasilla[filas, columnas]; // [FILAS, COLUMNAS]
+
+            for (int fila = 0; fila < filas; fila++)
+            {
+                for (int col = 0; col < columnas; col++)
+                {
+                    tablero[fila, col] = TipoCasilla.VACIA; // Consistente con [fila,col]
+                }
+            }
+        }
+
+        public void ColocaFicha(int col, int fil, TipoCasilla tipo)
+        {
+            try
+            {
+                if(tablero[fil, col] == TipoCasilla.VACIA) tablero[fil, col] = tipo;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool TableroCompleto()
+        {
+            int fila = 0, col = 0;
+            bool completo = false;
+            int vacio = 0;
+
+            while (fila < filas)
+            {
+                col = 0;
+                while (col < columnas)
+                {
+                    if (tablero[fila, col] == TipoCasilla.VACIA)
+                    {
+                        vacio++;
+                    }
+                    col++;
+                }
+                fila++;
+            }
+            return completo = vacio == 0;
+        }
+
+        public TipoCasilla GetCasilla(int col, int fil)
+        {
+            try
+            {
+                return tablero[fil, col];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return TipoCasilla.VACIA;
+            }
+        }
+
+        public bool CaeFicha(int n, TipoCasilla tipo) //Coloca la ficha en la columna n, y si hay algo entonces se pone arriba
+        {
+            int fil = 0;
+            bool cae = false;
+
+            if (tipo != TipoCasilla.VACIA)
+            {
+                try
+                {
+                    if (tablero[fil, n] == TipoCasilla.VACIA) tablero[fil, n] = tipo;
+                    else if (n < columnas)
+                    {
+                        while (fil < filas && !cae)
+                        {
+                            fil++;
+                            if (tablero[fil, n] == TipoCasilla.VACIA)
+                            {
+                                tablero[fil, n] = tipo;
+                                cae = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return cae;
+
+        }
+    }
+
+
+    public enum TipoCasilla //COMPLETADO
+    {
+        VACIA,
+        ROJA,
+        AMARILLA
+    }
+
+    public class Reglas //COMPLETADO
+    {
+        public TipoCasilla QuienEmpieza()
+        {
+            return TipoCasilla.AMARILLA;
+        }
+
+        public TipoCasilla ColorContrario(TipoCasilla tipo)
+        {
+            if (tipo == TipoCasilla.VACIA)
+            {
+                return tipo;
+            }
+            else if (tipo == TipoCasilla.ROJA)
+            {
+                return TipoCasilla.AMARILLA;
+            }
+            else return TipoCasilla.ROJA;
+        }
+
+        public bool PuedePoner(Tablero t, int columna)
+        {
+            return t.GetCasilla(columna, 5) == TipoCasilla.VACIA;
+        }
+
+        /// <summary>
+        /// Señalas cuantas casillas estan seguidas
+        /// ox y xy son para ver la columna y fila y de esta localizar la ficha colocada
+        /// 
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <param name="ox"></param>
+        /// <param name="oy"></param>
+        /// <param name="incrx"></param>
+        /// <param name="incry"></param>
+        /// <returns></returns>
+        public int CuantasSeguidas(Tablero tab, int ox, int oy, int incrx, int incry) //usado mas tarde como privado para el metodo publico de Gana, incrementa de x y y para un for en gana que recorre el tablero será (1,1)(1,0)(-1,0)(-1,1)(0,1)(0,-1)
+        {
+            TipoCasilla expected = tab.GetCasilla(ox, oy);
+            int ret = 1;
+
+            while (tab.GetCasilla(ox + incrx, oy + incry) == expected)
+            {
+                ox += incrx;
+                oy += incry;
+                ++ret;
+            }
+            return ret;
+        }
+         public Tablero TableroInicial()
+        {
+            Tablero tab = new Tablero();
+            return tab;
+        }
+
+        //public bool Gana(Tablero tab, TipoCasilla tipo) //entra en esta la tabla y el que juega en este turno
+        //{
+            
+        //}
+
+    }
+
+}
---
+++ GUI.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conecta4
+{
+    public class GUI
+    {
+        /// <summary>
+        /// Escribe el tablero y a quién le toca. Si el turno es TipoCasilla.VACIA,
+        /// entonces no escribe a quién le toca :-)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="turno"></param>
+        public static void PintaTablero(Tablero t, TipoCasilla turno)
+        {
+            for (int y = 5; y >= 0; --y)
+            {
+                string line = "|";
+                for (int x = 0; x < 7; ++x)
+                {
+                    line += ToChar(t.GetCasilla(x, y));
+                }
+                line += "|";
+
+                Console.WriteLine(line);
+            }
+
+            for (int x = 0; x < 2 + 7; ++x)
+                Console.Write('=');
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (turno != TipoCasilla.VACIA)
+            {
+                Console.WriteLine("Juegan " + ToUserFriendly(turno) + " (" + ToChar(turno) + ")");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Pregunta por una columna. No comprueba que el valor introducido esté en el rango.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="turno"></param>
+        /// <returns></returns>
+        public static int PideColumna()
+        {
+            return LeeEntero("Indica la columna donde colocar la ficha: ");
+        }
+
+        public static void EscribeGanador(Tablero t, TipoCasilla ganador)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            PintaTablero(t, TipoCasilla.VACIA);
+
+            Console.WriteLine("FIN DE JUEGO. Ganan " + ToUserFriendly(ganador));
+        }
+
+        public static bool JugarOtra()
+        {
+            Console.WriteLine();
+            string respuesta;
+            do
+            {
+                Console.Write("Otra partida? (S/N): ");
+                respuesta = Console.ReadLine();
+                respuesta = respuesta.ToLower();
+            } while ((respuesta != "s") && (respuesta != "n"));
+
+            return respuesta == "s";
+        }
+        private static char ToChar(TipoCasilla t)
+        {
+            switch (t)
+            {
+                case TipoCasilla.VACIA: return ' ';
+                case TipoCasilla.ROJA: return 'X';
+                case TipoCasilla.AMARILLA: return 'O';
+            }
+            return ' '; // Aquí no llegará nunca...
+        }
+
+        private static string ToUserFriendly(TipoCasilla t)
+        {
+            switch (t)
+            {
+                case TipoCasilla.ROJA: return "rojas";
+                case TipoCasilla.AMARILLA: return "amarillas";
+            }
+            return "";
+        }
+
+        private static int LeeEntero(string prompt)
+        {
+            int ret;
+            string input;
+            do
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine();
+            } while (!Int32.TryParse(input, out ret));
+
+            return ret;
+        }
+
+        public static void Main()
+        {
+            Reglas reglas;
+            do
+            {
+                reglas = new Reglas();
+                Tablero tablero = reglas.TableroInicial();
+                TipoCasilla turno = reglas.QuienEmpieza();
+                turno = reglas.ColorContrario(turno); // El "último en poner" fue el contrario al que empieza
+                do
+                {
+                    turno = reglas.ColorContrario(turno);
+                    GUI.PintaTablero(tablero, turno);
+                    int col;
+                    do
+                    {
+                        col = GUI.PideColumna();
+                    } while (!tablero.CaeFicha(col - 1, turno));
+
+                } while (!reglas.Gana(tablero, turno) && !tablero.TableroCompleto());
+                if (!tablero.TableroCompleto())
+                    GUI.EscribeGanador(tablero, turno);
+                else
+                {
+                    GUI.PintaTablero(tablero, TipoCasilla.VACIA);
+                    Console.WriteLine(" ==== TABLAS ====");
+                }
+            } while (GUI.JugarOtra());
+        }
+
+    }
+}
